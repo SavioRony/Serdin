35d79f3f6c08e7012fda06d32d07b308ee98b7c5
Savio Rony <savio_ls@hotmail.com>
2021-08-15 10:12:43 -0300

    Criação do projeto

---
+++ src/main/java/br/com/serdin/SerdinApplication.java
@@ -0,0 +1,21 @@
+package br.com.serdin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EntityScan(basePackages = "br.com.serdin.model")
+@ComponentScan(basePackages = { "br.*" })
+@EnableJpaRepositories(basePackages = { "br.*" })
+@EnableTransactionManagement
+public class SerdinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerdinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/serdin/SerdinApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serdin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerdinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
